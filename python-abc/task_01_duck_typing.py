73d559c7d71b8058191f92ad982323c6ec407fb8
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-06-26 15:45:26 +0000

    task_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -24,7 +24,7 @@ class Circle(Shape):
 
     def __init__(self, radius):
         """Initialize a circle with a radius."""
-        self.radius = radius
+        self.radius = abs(radius)
 
     def area(self):
         """Return the area of the circle."""
